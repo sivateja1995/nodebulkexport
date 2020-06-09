6d0d4697c1ce3ee02ebc5053d524998e0f76f725
sivateja <sivad@tinydata.in>
2020-06-09 17:18:32 +0530

    version2.0

--- exceltodatabase/exceltodbRoutes.js
+++ exceltodatabase/exceltodbRoutes.js
@@ -1,16 +1,11 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const router = express.Router();
-xlsxj = require("xlsx-to-json");
+const xlsxj = require("xlsx-to-json");
 const path = require('path');
-const exceltodb = require('../models/exceltodb');
-const fs = require('fs');
 
 var files = path.join(__dirname, "/files/");
 
 xlsxj({
-    input: path.join(files, "locations.xlsx"),
-    output: path.join(files, "location.json")
+    input: path.join(files, "bcd.xlsx"),
+    output: path.join(files, "bcd.json")
 }, function(err, result) {
     if (err) {
         console.log(err);
--- exceltodatabase/roleSavedb.js
+++ exceltodatabase/roleSavedb.js
@@ -3,44 +3,32 @@ const fs = require('fs');
 const path = require('path');
 const { stringify } = require('querystring');
 
-mongoose.connect('mongodb+srv://tars1729:tars1729@mycluster-eljsg.azure.mongodb.net/healthapp?authSource=admin&replicaSet=MyCluster-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass%20Community&retryWrites=true&ssl=true', {
-    useCreateIndex: true,
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-});
-
 // mongoose.connect('mongodb://localhost:27017/healthapp', {
 //     useCreateIndex: true,
 //     useNewUrlParser: true,
 //     useUnifiedTopology: true
 // });
 
+mongoose.connect('mongodb+srv://tars1729:tars1729@mycluster-eljsg.azure.mongodb.net/healthapp?authSource=admin&replicaSet=MyCluster-shard-0&w=majority&readPreference=primary&appname=MongoDB%20Compass%20Community&retryWrites=true&ssl=true', {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
 const jsonSchema = mongoose.Schema({
-    region: {
+    role: {
         type: String
     },
-    country: {
-        type: String
-    },
-    locationName: {
-        type: String
-    },
-    locationType: {
-        type: String
-    },
-    languageCode: {
-        type: String
-    },
-    priorityNumber: {
+
+    languageKey: {
         type: String
     }
-
 });
-const location = mongoose.model("location", jsonSchema);
+const location = mongoose.model("role", jsonSchema);
 
 var files = path.join(__dirname, "/files/");
 
-const jsonfile = path.join(files, '/bcd.json');
+const jsonfile = path.join(files, '/roles.json');
 let LocationData = fs.readFileSync(jsonfile, 'utf-8');
 let Locations = JSON.parse(LocationData);
 console.log(Locations);
